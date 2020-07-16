86a620818448e972de496729c69266cab7fe843e
Heiko Mueller <heiko.muller@gmail.com>
2020-07-16 14:50:01 -0400

    Change post-processing plot

--- template/code/postproc.py
+++ template/code/postproc.py
@@ -1,12 +1,7 @@
-"""Post-processing code for hello workd Demo. This code is included with
-flowServ for testing purposes only.
-"""
-
-from __future__ import absolute_import, division, print_function
+from collections import Counter
 
 import argparse
 import errno
-import numpy as np
 import matplotlib.pyplot as plt
 import os
 import sys
@@ -15,84 +10,51 @@ import time
 from flowserv.service.postproc.client import Runs
 
 
-def main(rundir, outputfile):
-    """Write greeting for every name in a given input file to the output file.
-    The optional waiting period delays the output between each input name.
-
+def main(rundir, outputfile=None):
+    """Create a plot showing the frequency of the 25 most frequent n-grams in
+    the greeting files of all runs. Counts only those n-grams that do not contain
+    a whitespace character.
     """
-    # Count frequency of n-grams for each run
-    keys = set()
-    results = list()
+    # Count frequency of n-grams for all runs.
+    ngrams = Counter()
     for run in Runs(rundir):
-        # Get five most frequent n-grams
-        ngrams = dict()
         with open(run.get_file('results/greetings.txt'), 'r') as f:
             for line in f:
                 line = line.strip()
                 if len(line) >= 3:
                     for i in range(len(line) - 2):
-                        ng = line[i:i + 3]
-                        ngrams[ng] = ngrams.get(ng, 0) + 1
-        candidates = [(n, ngrams[n]) for n in ngrams]
-        candidates.sort(key=lambda p: p[1], reverse=True)
-        if len(candidates) > 5:
-            candidates = candidates[:5]
-        results.append((run.name, candidates))
-        # Add n-grams to global set of result keys
-        for c in candidates:
-            keys.add(c[0])
+                        ng = line[i:i + 3].upper()
+                        if not ' ' in ng:
+                            ngrams[ng] += 1
         # Delay execution to allow for testing running post-processing
         # workflows
-        time.sleep(10)
-    keys = sorted(list(keys))
-    # Write plot output file. Ensure that output directory exists:
-    # influenced by http://stackoverflow.com/a/12517490
-    if not os.path.exists(os.path.dirname(outputfile)):
-        try:
-            os.makedirs(os.path.dirname(outputfile))
-        except OSError as exc:  # guard against race condition
-            if exc.errno != errno.EEXIST:
-                raise
-    # create plot
-    fig, ax = plt.subplots()
-    n_groups = len(keys)
-    index = np.arange(n_groups)
-    bar_width = 0.35
-    opacity = 0.8
-    colors = ['b', 'g', 'r', 'c', 'm', 'y']
-    series = 0
-    for name, candidates in results:
-        ngrams = dict()
-        for key, count in candidates:
-            ngrams[key] = count
-        values = list()
-        for key in keys:
-            values.append(ngrams.get(key, 0))
-        plt.bar(
-            index,
-            values,
-            bar_width,
-            alpha=opacity,
-            color=colors[series % len(colors)],
-            label=name
-        )
-        index = index + bar_width
-        series += 1
-    plt.xlabel('3-grams')
-    plt.ylabel('Counts')
-    plt.xticks(index, keys)
-    plt.legend()
-    plt.tight_layout()
-    plt.savefig(outputfile)
-
-
-if __name__ == '__main__':
-    args = sys.argv[1:]
-
-    parser = argparse.ArgumentParser()
-    parser.add_argument("-r", "--runs", required=True)
-    parser.add_argument("-o", "--outputfile", required=True)
-
-    parsed_args = parser.parse_args(args)
-
-    main(rundir=parsed_args.runs, outputfile=parsed_args.outputfile)
+        time.sleep(0)
+    # Create plot
+    x = list()
+    y = list()
+    for ngram, count in ngrams.most_common(25):
+        x.append(ngram)
+        y.append(count)
+    x = x[::-1]
+    y = y[::-1]
+    plt.style.use('ggplot')
+    x_pos = [i for i, _ in enumerate(x)]
+    plt.figure(figsize=(10, 15))
+    plt.barh(x_pos, y, color='green')
+    plt.xlabel("Count")
+    plt.title("Top-25 n-grams by frequency")
+    plt.yticks(x_pos, x)
+    # Write to file if output directory is given. Otherwise
+    # show the plot.
+    if outputfile is not None:
+        # Write plot output file. Ensure that output directory exists:
+        # influenced by http://stackoverflow.com/a/12517490
+        if not os.path.exists(os.path.dirname(outputfile)):
+            try:
+                os.makedirs(os.path.dirname(outputfile))
+            except OSError as exc:  # guard against race condition
+                if exc.errno != errno.EEXIST:
+                    raise
+        plt.savefig(outputfile)
+    else:
+        plt.show()
